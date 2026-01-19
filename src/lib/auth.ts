72bdfb8cafe0d75e33d17ba27ece23b729a3421b
rishabhtrivedi3-2 <trivedirishabh27@gmail.com>
2026-01-20 03:05:39 +0530

    fix: vercel build node + swc + bcrypt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 import { prisma } from "@/lib/prisma"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
